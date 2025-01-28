eb5f2bcd70101440e981c429ae7e576b84395758
Keona <124313409+keona-dc@users.noreply.github.com>
2025-01-28 12:53:00 -0700

    Update

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <div style={{margin: '40px'}}>
+          <h1 style={{fontSize:'30px'}}>Shopping List</h1>
+          <StudentInfo />
+      </div>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1 style={{fontSize: '30px'}}>Keona Dela Cruz</h1>        
+            <a href="https://github.com/keona-dc" target="_blank">https://github.com/keona-dc</a>
+        </div>
+    );
+};
+
+export default StudentInfo;
